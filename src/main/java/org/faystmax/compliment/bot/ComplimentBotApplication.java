9c676ac53e4906b37e13e7a4899b82188d5d82e7
Amosov Maxim <faystmax@gmail.com>
2024-03-10 12:13:46 +0300

    Remove import

--- src/main/java/org/faystmax/compliment/bot/ComplimentBotApplication.java
+++ src/main/java/org/faystmax/compliment/bot/ComplimentBotApplication.java
@@ -16,7 +16,6 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
 
